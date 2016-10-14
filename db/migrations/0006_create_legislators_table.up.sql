c8f17b564779c61f5c53f95c610ba8d982a8d1d1
Hong <jimmy600kimo@hotmail.com>
2016-10-14 22:56:32 +0800

    feat(migration): create legislators table

--- db/migrations/0006_create_legislators_table.up.sql
+++ db/migrations/0006_create_legislators_table.up.sql
@@ -1,5 +1,14 @@
 CREATE TABLE legislators(
-	id INT PRIMARY KEY,
-	name VARCHAR(60),
-	english_name VARCHAR(60)
+	id 				INT 			PRIMARY KEY NOT NULL,
+	name 			VARCHAR(60)		NOT NULL,
+	english_name 	VARCHAR(60),
+	gender 			CHAR(1)			NOT NULL,
+	email VARCHAR(255),
+	experience VARCHAR(1023),
+	facebook VARCHAR(255),
+	wiki VARCHAR(511),
+	line VARCHAR(255),
+	party_id INT REFERENCES parties(id),
+	party_group_id INt REFERENCES party_groups(id),
+	electoral_district_id INT REFERENCES electoral_districts(id)
 );
