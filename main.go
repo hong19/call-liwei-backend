f53824020c9b8851eda64c1102e1b1f7b44dad25
Hong <jimmy600kimo@hotmail.com>
2016-10-15 10:21:55 +0800

    feat(main): read csv a line once a time

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"io"
 	"io/ioutil"
-	"fmt"	
-	//"encoding/csv"
+	"log"
+	"strings"
 )
-
 
 func main() {
 
-    fmt.Printf("program start\n")
+	fmt.Printf("parse start\n")
 
-    dat, err := ioutil.ReadFile("legislator_info.csv")
-    check(err)
-    fmt.Print(string(dat))
+	dat, err := ioutil.ReadFile("legislator_info.csv")
+	check(err)
+
+	r := csv.NewReader(strings.NewReader(string(dat)))
+
+	//read out header line
+	r.Read()
+
+	i := 0
+	for i = 0; i < 2; i++ {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(record)
+	}
+
+	fmt.Printf("parse end\n")
 }
 
-
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
